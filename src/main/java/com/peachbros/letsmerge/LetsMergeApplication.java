d4dc00073ed101138f37d401a685e7299d4a8ac8
yebink <yebin825@gmail.com>
2020-04-24 17:47:04 +0900

    project init

---
+++ src/main/java/com/peachbros/letsmerge/LetsMergeApplication.java
@@ -0,0 +1,13 @@
+package com.peachbros.letsmerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsMergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsMergeApplication.class, args);
+	}
+
+}
