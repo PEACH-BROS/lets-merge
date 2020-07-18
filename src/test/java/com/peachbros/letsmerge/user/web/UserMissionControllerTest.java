5afb762720d3e385bc641d7e9fb33a210ea8604d
yebink <yebin825@gmail.com>
2020-07-19 03:21:05 +0900

    lets-merge-14 test: 사용자의 신청한 미션 조회 API 테스트 추가

--- src/test/java/com/peachbros/letsmerge/user/web/UserMissionControllerTest.java
+++ src/test/java/com/peachbros/letsmerge/user/web/UserMissionControllerTest.java
@@ -1,6 +1,12 @@
 package com.peachbros.letsmerge.user.web;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.peachbros.letsmerge.config.auth.SecurityConfig;
+import com.peachbros.letsmerge.core.dto.StandardResponse;
+import com.peachbros.letsmerge.mission.model.domain.Mission;
+import com.peachbros.letsmerge.mission.service.dto.MissionsResponse;
 import com.peachbros.letsmerge.user.service.UserMissionService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,16 +15,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,11 +41,14 @@ class UserMissionControllerTest {
 
     private MockMvc mockMvc;
 
+    private ObjectMapper objectMapper;
+
     @BeforeEach
     void setUp(WebApplicationContext webApplicationContext) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .addFilter(new CharacterEncodingFilter("UTF-8", true))
                 .build();
+        this.objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
     }
 
     @DisplayName("사용자의 미션 신청")
@@ -63,10 +76,30 @@ class UserMissionControllerTest {
     }
 
     @Test
-    void getAssignedMissions() {
+    void getAssignedMissions() throws Exception {
+        when(userMissionService.getAssignedMissions(anyLong())).thenReturn(dummyMissionsResponse());
+        MvcResult mvcResult = this.mockMvc.perform(get("/api/v1/users/1/missions/assigned")
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        StandardResponse<MissionsResponse> missionsResponse = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<StandardResponse<MissionsResponse>>() {
+        });
+
+        verify(userMissionService).getAssignedMissions(anyLong());
     }
 
     @Test
     void getAssignableMissions() {
+    }
+
+    private MissionsResponse dummyMissionsResponse() {
+        List<Mission> missions = Arrays.asList(dummyMission(), dummyMission());
+        return MissionsResponse.of(missions);
+    }
+
+    private Mission dummyMission() {
+        return new Mission("미션", LocalDateTime.of(2020, 5, 1, 21, 0), LocalDateTime.of(2020, 5, 8, 21, 0));
     }
 }
