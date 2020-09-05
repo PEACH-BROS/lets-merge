6f89b81cc064dc3d351384d83c43e43cb7478fcc
yebink <yebin825@gmail.com>
2020-09-06 05:41:49 +0900

    lets-merge-28 chore: admin back/client front port 지정

--- lets-merge-client-front/vue.config.js
+++ lets-merge-client-front/vue.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const port = 3000;
+const proxyPort = 8080;
 
 module.exports = {
   transpileDependencies: ["vuetify"],
@@ -6,6 +8,16 @@ module.exports = {
     resolve: {
       alias: {
         "@": path.join(__dirname, "src/"),
+      },
+    },
+  },
+  devServer: {
+    port: port,
+    proxy: {
+      "/": {
+        target: `http://localhost:${proxyPort}`,
+        ws: true,
+        changeOrigin: true,
       },
     },
   },
